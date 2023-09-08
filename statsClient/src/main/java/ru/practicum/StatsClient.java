32346d337231b0efde706250f6c8fce9d36556b9
bjeel <j.kreydich@gmail.com>
2023-09-08 22:06:16 +0300

    finish stats

--- statsClient/src/main/java/ru/practicum/StatsClient.java
+++
@@ -1,9 +0,0 @@
-package ru.practicum;
-
-import org.springframework.boot.SpringApplication;
-
-public class StatsClient {
-  public static void main(String[] args) {
-    SpringApplication.run(StatsClient.class, args);
-  }
-}
